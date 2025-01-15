06fb42b4ff87f5484051fdc383c6f352ac2383b2
Prabhat Yadav <133522303+Prabhatyadav60@users.noreply.github.com>
2025-01-15 17:56:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function saveTasks(tasks) {
     localStorage.setItem('todoList', JSON.stringify(data));
 }
 
+
 function loadTasks() {
     const data = JSON.parse(localStorage.getItem('todoList'));
     if (data) {
@@ -15,6 +16,7 @@ function loadTasks() {
     }
     return [];
 }
+
 
 function renderTasks(tasks) {
     const taskList = document.getElementById('taskList');
@@ -37,7 +39,7 @@ function renderTasks(tasks) {
             task.completed = checkbox.checked;
             saveTasks(tasks);
             renderTasks(tasks);
-            if (checkbox.checked) triggerConfetti();
+            if (checkbox.checked) showLottieAnimation(li);
         };
 
         const span = document.createElement('span');
@@ -60,11 +62,50 @@ function renderTasks(tasks) {
     });
 }
 
+
+function showLottieAnimation(parentElement) {
+    const lottie = document.createElement('lottie-player');
+    lottie.setAttribute('src', 'https://lottie.host/embed/00770f2e-13a1-43ad-a45f-7d3868e95637/WJFul7gYpG.json');
+    lottie.setAttribute('background', 'transparent');
+    lottie.setAttribute('speed', '1');
+    lottie.setAttribute('autoplay', 'true');
+    lottie.style.width = '50px';
+    lottie.style.height = '50px';
+    lottie.style.marginLeft = '10px';
+
+    parentElement.appendChild(lottie);
+
+
+    setTimeout(() => {
+        parentElement.removeChild(lottie);
+    }, 1500);
+}
+
+
 function displayDate() {
     const currentDate = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     document.getElementById('currentDate').textContent = currentDate.toLocaleDateString(undefined, options);
 }
+
+
+function showDeleteGif() {
+    const gifContainer = document.getElementById('deleteGifContainer');
+    gifContainer.innerHTML = `<img src="https://media.giphy.com/media/3orieRYTXkNkUG1UMU/giphy.gif" alt="Deleted" />`;
+    setTimeout(() => {
+        gifContainer.innerHTML = '';
+    }, 2000);
+}
+
+
+function triggerConfetti() {
+    const confettiCanvas = document.getElementById('confettiCanvas');
+    const confetti = new JSConfetti({ canvas: confettiCanvas });
+    confetti.addConfetti({
+        confettiColors: ['#9eeb34', '#fff'],
+    });
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     let tasks = loadTasks();
@@ -72,6 +113,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const taskInput = document.getElementById('taskInput');
     displayDate();
+
 
     taskInput.addEventListener('keypress', (e) => {
         if (e.key === 'Enter') {
